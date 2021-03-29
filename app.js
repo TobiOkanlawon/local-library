7507d0129609f08abc2842d15aadc27c0a163495
Tobi Okanlawon <tobiinlondon34@gmail.com>
2021-03-29 18:08:03 +0100

    Add skeleton code for library app

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const password = require("./secrets");
+const { password } = require("./secrets");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
